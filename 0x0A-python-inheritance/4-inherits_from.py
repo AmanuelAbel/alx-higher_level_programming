9fe9b4d4c5d3b89d62baa7dd4adf446944c071b3
AmanuelAbel <amanabel2015@gmail.com>
2022-07-05 12:03:10 +1400

    inheritance in python

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-Module 2-is_same_class.py
-same instance
-and methods
-"""
+""" check if an object is an instance of a class
+    of an instance of an inherited class """
 
 
 def inherits_from(obj, a_class):
-    """ is the objec is exactly an instance of the specified class """
-    return (type(obj) is not a_class and issubclass(type(obj), a_class))
+    """ function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class ; otherwise False. """
 
+    return isinstance(obj, a_class) and type(obj) != a_class
