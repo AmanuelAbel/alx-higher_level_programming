204aaf95b729a56cd555ca0dad01183b1fe43cc6
AmanuelAbel <amanabel2015@gmail.com>
2022-07-05 12:40:29 +1400

    inheritance in python

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
-""" class BaseGeometry """
+"""
+Module 9-rectangle
+Contains parent class BaseGeometry
+with public instance method area and integer_validation
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent,
+extends parent's area method and prints with __str__
+"""
 
 
-class BaseGeometry:
-    """ BaseGeometry class """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ class Rectangle that defines a rectangle """
+    """inherits from BaseGeometry
+    Methods:
+        __init__(self, width, height)
+        area(self)
+        __str__
+    """
 
     def __init__(self, width, height):
-        """ initialize a Rectangle
-        args1: width = width of Rectangle
-        args2: height = height of Rectangle """
+        """validate and initialize width and height
+        Args:
+            width (int): private
+            height (int): private
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """extends parent's empty method and returns area"""
         return self.__width * self.__height
 
     def __str__(self):
-        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
-
+        """prints [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__width, self.__height)
