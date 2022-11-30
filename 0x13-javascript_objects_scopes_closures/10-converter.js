fee632cc8d4579c8e80124a944f000c8c67e77cd
AmanuelAbel <amanabel2015@gmail.com>
2022-11-30 19:26:27 +1400

    commit

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-exports.converter = function (base) {
-  return function (num) {
-    return num.toString(base);
-  };
-};#!/usr/bin/node
+// Converts a number from base 10 to another base passed as argument
+// with Closure
 
 exports.converter = function (base) {
-  return function (num) {
-    return num.toString(base);
-  };
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
 };
